85022e7c130129aa562686dcaa914054b85f67cc
PavlePetrovic <pavlep275@gmail.com>
2022-07-08 15:21:08 +0200

    Update site

--- script.js
+++ script.js
@@ -1,41 +1,152 @@
-function getData(){
-    axios.get('https://api.punkapi.com/v2/beers')
+let selectView = document.getElementById('selectView');
+let leftArrow = document.getElementById('left')
+let rightArrow = document.getElementById('right')
+let page1 = document.getElementById('page-1')
+let page2 = document.getElementById('page-2')
+let page3 = document.getElementById('page-3')
+
+let pageNumber = 1
+let perPage = 10
+
+let axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+
+selectView.addEventListener('click', () => {
+    let selectViewValue = selectView.options[selectView.selectedIndex].value;
+    selectViewValue = parseInt(selectViewValue)
+
+    if(selectViewValue === 10){
+        perPage = 10
+        axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+        getData(axiosURL)
+    } else if(selectViewValue === 20){
+        perPage = 20
+        axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+        getData(axiosURL)
+    } else if(selectViewValue === 30){
+        perPage = 30
+        axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+        getData(axiosURL)
+    }
+})
+
+leftArrow.addEventListener('click', () => {
+    if(pageNumber === 1){
+        pageNumber = 1
+    }else {
+        pageNumber = pageNumber - 1
+        axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+        getData(axiosURL)
+    }
+
+    if(pageNumber === 1){
+        page1.classList.add('clicked')
+        page2.classList.remove('clicked')
+        page3.classList.remove('clicked')
+    } else if(pageNumber === 2){
+        page1.classList.remove('clicked')
+        page2.classList.add('clicked')
+        page3.classList.remove('clicked')
+    } else if(pageNumber === 3){
+        page1.classList.remove('clicked')
+        page2.classList.remove('clicked')
+        page3.classList.add('clicked')
+    }
+})
+
+rightArrow.addEventListener('click', () => {
+    if(pageNumber === 3){
+        pageNumber = 3
+    }else {
+        pageNumber = pageNumber + 1
+        axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+        getData(axiosURL)
+    }
+
+    if(pageNumber === 1){
+        page1.classList.add('clicked')
+        page2.classList.remove('clicked')
+        page3.classList.remove('clicked')
+    } else if(pageNumber === 2){
+        page1.classList.remove('clicked')
+        page2.classList.add('clicked')
+        page3.classList.remove('clicked')
+    } else if(pageNumber === 3){
+        page1.classList.remove('clicked')
+        page2.classList.remove('clicked')
+        page3.classList.add('clicked')
+    }
+})
+
+page1.addEventListener('click', () => {
+    pageNumber = 1
+    axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+    getData(axiosURL)
+
+    page1.classList.add('clicked')
+    page2.classList.remove('clicked')
+    page3.classList.remove('clicked')
+})
+
+page2.addEventListener('click', () => {
+    pageNumber = 2
+    axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+    getData(axiosURL)
+
+    page1.classList.remove('clicked')
+    page2.classList.add('clicked')
+    page3.classList.remove('clicked')
+})
+
+page3.addEventListener('click', () => {
+    pageNumber = 3
+    axiosURL = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=${perPage}`
+    getData(axiosURL)
+
+    page1.classList.remove('clicked')
+    page2.classList.remove('clicked')
+    page3.classList.add('clicked')
+})
+
+function getData(url) {
+    axios.get(url)
         .then(res => {
-            showTemplate(res.data);
-            showTableShowList()
-            search(res.data)
+            showTemplate(res.data)
+            onDisplayViewChange()
+            showBeerModal(res.data)
+            onSearch(res.data)
             addToCart(res.data)
-            filter()
+            onBtnFilterClick()
         })
         .catch(err => {
             console.log(err);
         })
 }
-getData()
+getData(axiosURL) 
 
-function showTemplate(data){
-    let beers = data
-    
+function showTemplate(data) {
+
     let scriptTemplate = document.querySelector('#template')
     let contentBox = document.querySelector(".content-items")
-    
+
     let template = Handlebars.compile(scriptTemplate.innerHTML)
-    let filled = template(beers, {
+    let filled = template(data, {
         // Default je false, to znaci:
         // Ako imamo {name: "list <br> 3 </br>"}, on ce da ga konvertuje kao tekst, ceo string
         // A ako je true, onda kada dodje do html taga u stringu, on konvertuje html, u ovom slucaju bi 3 bilo boldovano
         noEscape: true
     })
-    contentBox.innerHTML = filled
-    
+
+    return contentBox.innerHTML = filled
+
     // Koraci
     // * Imamo podatke [data] iz kojih izvlacimo informacije
     // * Imamo varijablu template u kojoj kompajlujemo nas handlebar kod, koji smo napisali u html-u
     // * Imamo varijablu filled u kojoj smestamo kompajlovan kod - html prikaz
     // * Hvatamo element u kome zelimo da prikazemo kod i onda ga ubacujemo pomocu innerHTML
+
 }
 
-function search(data){
+function onSearch(data) {
     let searchBar = document.querySelector('input[type="search"]')
     let searchBtn = document.querySelector('.search-button')
     let snackbar = document.getElementById('snackbar')
@@ -51,7 +162,7 @@ function search(data){
             snackbar.innerText = "Beer name"
             showTemplate(data)
             addToCart(data)
-            showTableShowList()
+            onDisplayViewChange()
 
             setTimeout( function(){  
                 snackbar.className = snackbar.className.replace("show", ""); 
@@ -71,7 +182,8 @@ function search(data){
             })
             showTemplate(searchedBeer)
             addToCart(searchedBeer)
-            showTableShowList()
+            onDisplayViewChange()
+            showBeerModal(searchedBeer)
         }
 
         searchedBeer = []
@@ -82,11 +194,12 @@ function search(data){
         showTemplate(data)
         searchBar.value = ''
         addToCart(data)
-        showTableShowList()
+        onDisplayViewChange()
+        showBeerModal(data)
     })
 }
 
-function showTableShowList(){
+function onDisplayViewChange() {
     let showTable = document.querySelector('.show-table')
     let showList = document.querySelector('.show-list')
     let contentItems = document.querySelector('.content-items')
@@ -103,69 +216,69 @@ function showTableShowList(){
     showTable.addEventListener('click', () => {
         contentItems.style.flexDirection = 'row'
         beerBox.forEach(box => {
-            box.style.flexDirection = 'column'
-            box.style.width = '260px'
-            box.style.justifyContent = 'center'
-            box.style.padding = '0px'
+            box.classList.remove('beer-box-list')
+            box.classList.add('beer-box-grid')
         })
         beerBoxImg.forEach(img => {
-            img.style.marginTop = '15px'
-            img.style.marginInline = '0px'
+            img.classList.remove('img-list')
+            img.classList.add('img-grid')
         })
         beerDescription.forEach(desc => {
-            desc.style.width = '260px'
-            desc.style.alignItems = 'center'
-            desc.style.paddingLeft = '0px'
+            desc.classList.remove('beer-description-list')
+            desc.classList.add('beer-description-grid')
         })
         beerDescriptionH3.forEach(h => {
-            h.style.display = 'none'
+            h.classList.remove('h3-list')
+            h.classList.add('h3-grid')
         })
         beerDescriptionText.forEach(text => {
-            text.style.display = 'none'
+            text.classList.remove('description-text-list')
+            text.classList.add('description-text-grid')
         })
         beerPrice.forEach(price => {
-            price.style.color = '#BF431F'
+            price.classList.remove('price-list')
+            price.classList.add('price-grid')
         })
         addToCartBtn.forEach(btn => {
-            btn.style.width = '100%'
-            btn.style.marginTop = '0px'
+            btn.classList.remove('add-to-cart-list')
+            btn.classList.add('add-to-cart-grid')
         })
     })
 
     showList.addEventListener('click', () => {
         contentItems.style.flexDirection = 'column'
         beerBox.forEach(box => {
-            box.style.flexDirection = 'row'
-            box.style.width = '100%'
-            box.style.justifyContent = 'flex-start'
-            box.style.padding = '25px'
+            box.classList.remove('beer-box-grid')
+            box.classList.add('beer-box-list')
         })
         beerBoxImg.forEach(img => {
-            img.style.marginTop = '0px'
-            img.style.marginInline = '60px'
+            img.classList.remove('img-grid')
+            img.classList.add('img-list')
         })
         beerDescription.forEach(desc => {
-            desc.style.width = '100%'
-            desc.style.alignItems = 'flex-start'
-            desc.style.paddingLeft = '25px'
+            desc.classList.remove('beer-description-grid')
+            desc.classList.add('beer-description-list')
         })
         beerDescriptionH3.forEach(h => {
-            h.style.display = 'block'
+            h.classList.remove('h3-grid')
+            h.classList.add('h3-list')
         })
         beerDescriptionText.forEach(text => {
-            text.style.display = 'block'
+            text.classList.remove('description-text-grid')
+            text.classList.add('description-text-list')
         })
         beerPrice.forEach(price => {
-            price.style.color = '#44322c'
+            price.classList.remove('price-grid')
+            price.classList.add('price-list')
         })
         addToCartBtn.forEach(btn => {
-            btn.style.width = '170px'
-            btn.style.marginTop = '5px'
+            btn.classList.remove('add-to-cart-grid')
+            btn.classList.add('add-to-cart-list')
         })
     })
 }
 
-function addToCart(data){
+function addToCart(data) {
     let contentBox = document.querySelector(".content-items")
     let beerBox = contentBox.querySelectorAll('.beer-box')
     let cart = document.querySelector('.cart')
@@ -256,6 +369,7 @@ function addToCart(data){
             } else{
                 beersInCart.forEach(beer => {
                     if(beer.name === delItemName){
+                        totalPrice = JSON.parse(localStorage.getItem("totalPrice"))
                         totalPrice = totalPrice - beer.selectedBeerQuantity * beer.abv
                         total.innerText = `TOTAL: $${totalPrice.toFixed(1)}`
                         beer.selectedBeerQuantity = 1
@@ -270,18 +384,16 @@ function addToCart(data){
     })
 }
 
-function showTemplateCart(data){
-    let beers = data
-    
+function showTemplateCart(data) {
     let scriptTemplate = document.querySelector('#template-cart')
     let cart = document.querySelector(".cart-prices")
     
     let template = Handlebars.compile(scriptTemplate.innerHTML)
-    let filled = template(beers)
+    let filled = template(data)
     cart.innerHTML = filled
 }
 
-function priceFilter(data){
+function onInputPriceChange(data) {
     let priceSlider = document.getElementById('price-slider')
     let showPrice = document.querySelector('.show-price')
 
@@ -314,10 +426,11 @@ function priceFilter(data){
     
     showTemplate(beersInPriceRange)
     addToCart(data)
-    showTableShowList()
+    onDisplayViewChange()
+    showBeerModal(data)
 }
 
-function filter(){
+function onBtnFilterClick() {
     let filterBtn = document.querySelector('.btn-filter')
     let brewedAfter = document.getElementById('brewed-after')
     let brewedBefore = document.getElementById('brewed-before')
@@ -343,47 +456,22 @@ function filter(){
             }
         })
 
+        let url = axiosURL
+
         if(after && before && pickedFood){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_after=${afterMonth}-${afterYear}&brewed_before=${beforeMonth}-${beforeYear}&food=${pickedFood}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
+            url = `${url}&brewed_after=${afterMonth}-${afterYear}&brewed_before=${beforeMonth}-${beforeYear}&food=${pickedFood}`
+                
         } else if(after && before){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_after=${afterMonth}-${afterYear}&brewed_before=${beforeMonth}-${beforeYear}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
-        } else if(after && pickedFood){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_after=${afterMonth}-${afterYear}&food=${pickedFood}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
-        } else if(before && pickedFood){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_before=${beforeMonth}-${beforeYear}&food=${pickedFood}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
-        } else if(after){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_after=${afterMonth}-${afterYear}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
-        } else if(before){
-            axios.get(`https://api.punkapi.com/v2/beers?brewed_before=${beforeMonth}-${beforeYear}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
+            url = `${url}&brewed_after=${afterMonth}-${afterYear}&brewed_before=${beforeMonth}-${beforeYear}`
+               
         } else if(pickedFood){
-            axios.get(`https://api.punkapi.com/v2/beers?food=${pickedFood}`)
-                .then(res => {
-                    priceFilter(res.data)
-                })
-        } else{
-            axios.get(`https://api.punkapi.com/v2/beers`)
-            .then(res => {
-                priceFilter(res.data)
-            })
+            url = `${url}&food=${pickedFood}`
         }
+
+        axios.get(url)
+            .then(res => {
+                onInputPriceChange(res.data)
+            })
 
         resetSearch.style.display = 'none'
         searchBar.value = ''
@@ -401,11 +489,130 @@ function filter(){
         resetSearch.style.display = 'none'
         searchBar.value = ''
         resetFilters.style.display = 'none'
-        axios.get(`https://api.punkapi.com/v2/beers`)
+        axios.get(axiosURL)
             .then(res => {
                 showTemplate(res.data)
                 addToCart(res.data)
-                showTableShowList()
+                onDisplayViewChange()
+                showBeerModal(res.data)
             })
     })
 }
+
+function showBeerModal(data) {
+    let modal = document.querySelectorAll(".modal");
+    let beerBox = document.querySelectorAll('.beer-box')
+
+    beerBox.forEach(beer => {
+        beer.addEventListener('click', (e) => {
+            if(e.target.tagName != 'BUTTON'){ 
+                let clickedBeerName = beer.children[1].children[0].innerText
+                let clickedModal
+                let clickedBeerData 
+
+                modal.forEach(mod => {
+                    let modBeerName = mod.children[0].innerText
+                    if(clickedBeerName === modBeerName){
+                        mod.style.display = "block";
+                        clickedModal = mod
+                    }
+                })
+
+                data.forEach(beer => {
+                    if(beer.name === clickedBeerName){
+                        clickedBeerData = beer
+                    }
+                })
+
+                let quantityInput = clickedModal.querySelector('#quantity')
+                let upArrow = clickedModal.querySelector('#quantity-up')
+                let upDown = clickedModal.querySelector('#quantity-down')
+                
+                quantityInput.value = 1
+
+                upArrow.addEventListener('click', () => {
+                    console.log(quantityInput.value);
+                    let inputValue = parseInt(quantityInput.value)
+                    inputValue++
+                    quantityInput.value = inputValue
+                })
+
+                upDown.addEventListener('click', () => {
+                    let inputValue = parseInt(quantityInput.value)
+                    if(inputValue === 1){
+                        quantityInput.value = 1
+                    } else{
+                        inputValue--
+                        quantityInput.value = inputValue
+                    }
+                })
+
+                let cart = document.querySelector('.cart')
+                let cartPlaceholder = document.querySelector('.cart-placeholder')
+                let total = cart.querySelector('.total')
+                let addBtn = clickedModal.querySelector('.add-in-cart-modal')
+                
+                addBtn.addEventListener('click', () => {
+                    let beersInCart
+                    let totalPrice
+
+                    if(localStorage.getItem("totalPrice") === null){
+                        totalPrice = 0
+                    } else{
+                        totalPrice = JSON.parse(localStorage.getItem("totalPrice"))
+                    }
+
+                    if(localStorage.getItem("beersInCart") === null){
+                        beersInCart = []
+                    } else{
+                        beersInCart = JSON.parse(localStorage.getItem("beersInCart"))
+                    }
+
+                    showTemplateCart(JSON.parse(localStorage.getItem("beersInCart")))
+
+                    localStorage.setItem('beersInCart', JSON.stringify(beersInCart))
+                    localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
+
+                    let inputValue = parseInt(quantityInput.value)
+                              
+                    cartPlaceholder.innerText = ''
+
+                    let isBeerInCart = false
+
+                    beersInCart.forEach(beer => {
+                        if(beer.name === clickedBeerName){
+                            beer.selectedBeerQuantity =  beer.selectedBeerQuantity + inputValue
+                            let price = beer.abv
+                            totalPrice = price * inputValue + totalPrice
+                            isBeerInCart = true
+                            localStorage.setItem("beersInCart", JSON.stringify(beersInCart))
+                        } 
+                    })
+
+                    if(isBeerInCart === false){
+                        clickedBeerData.selectedBeerQuantity = inputValue
+                        beersInCart.push(clickedBeerData)
+                        let price = clickedBeerData.abv * inputValue
+                        totalPrice += price
+                        localStorage.setItem("beersInCart", JSON.stringify(beersInCart))
+                    }
+                    
+                    total.innerText = `TOTAL: $${totalPrice.toFixed(1)}`     
+                    
+                    localStorage.setItem('totalPrice', JSON.stringify(totalPrice))
+                    showTemplateCart(JSON.parse(localStorage.getItem("beersInCart")))
+                })
+
+                window.addEventListener('click', (e) => {
+                    addToCart(data)
+                    if (e.target == clickedModal) {
+                        clickedModal.style.display = "none";
+                        document.location.reload()
+                    }
+                })
+            }
+        })
+    })
+}
+
+
